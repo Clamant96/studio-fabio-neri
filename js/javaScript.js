9d142cabd9ed4a386e926344baabf098bbe3137d
Kevin Alec Neri Lazzarotto <49625082+Clamant96@users.noreply.github.com>
2020-11-20 10:10:17 -0300

    Otimizacao de aplicacao

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,28 +1,59 @@
-document.getElementById('menu').onclick = function() {opcaoMenu()};
+function myFunction(x) {
+    document.getElementById('menu').onclick = function() {opcaoMenu()};
 
-var menu = document.getElementById('sideBar');
+    var menu = document.getElementById('sideBar');
 
-var contador = 0;
+    var contador = 0;
 
-function opcaoMenu(){
-    switch(contador % 2) {
-        case 1:
-            menu.style.marginLeft = '-22%';
-
-            contador = 0;
-
-        break;
-
-        case 0:
-            menu.style.marginLeft = '0%';
-
-            contador = 1;
-
-        break;
-
-        default:
-            console.log('Erro na aplicacao!');
-        break;
-
+    if (x.matches) { // If media query matches
+        function opcaoMenu(){
+            switch(contador % 2) {
+                case 1:
+                    menu.style.marginLeft = '-32%';
+        
+                    contador = 0;
+        
+                break;
+        
+                case 0:
+                    menu.style.marginLeft = '0%';
+        
+                    contador = 1;
+        
+                break;
+        
+                default:
+                    console.log('Erro na aplicacao!');
+                break;
+        
+            }
+        }
+    } else {
+        function opcaoMenu(){
+            switch(contador % 2) {
+                case 1:
+                    menu.style.marginLeft = '-22%';
+        
+                    contador = 0;
+        
+                break;
+        
+                case 0:
+                    menu.style.marginLeft = '0%';
+        
+                    contador = 1;
+        
+                break;
+        
+                default:
+                    console.log('Erro na aplicacao!');
+                break;
+        
+            }
+        }
     }
-}
+  }
+  
+  var x = window.matchMedia("(max-width: 700px)");
+  myFunction(x);
+  x.addListener(myFunction);
